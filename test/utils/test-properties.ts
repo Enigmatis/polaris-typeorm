20a5515c2f86946b037609249b2d7a790335029c
chenshoo <chenshooker@gmail.com>
2019-10-29 16:36:59 +0200

    Update test-properties.ts

--- test/utils/test-properties.ts
+++ test/utils/test-properties.ts
@@ -6,7 +6,7 @@ import {ApplicationLogProperties, LoggerConfiguration} from "@enigmatis/polaris-
 const path = require('path');
 export const connectionOptions: ConnectionOptions = {
     type: "postgres",
-    url: process.env.connectionString ? process.env.connectionString : "",
+    url: process.env.CONNECTION_STRING ? process.env.connectionString : "",
     entities: [
         path.resolve(__dirname, '..') + '/dal/*.ts',
         CommonModel,
