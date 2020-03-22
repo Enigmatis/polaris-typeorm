fa0dcafaf1e733a3fb7de74150a1ee63582c87a4
Arik F <furmanmail@gmail.com>
2020-03-22 15:38:31 +0200

    fix: adds deleted where to relations too

--- src/handlers/find-handler.ts
+++ src/handlers/find-handler.ts
@@ -1,6 +1,6 @@
-import { PolarisRequestHeaders } from '@enigmatis/polaris-common';
-import { FindConditions, In, MoreThan } from 'typeorm';
-import { PolarisFindManyOptions, PolarisFindOneOptions } from '..';
+import {PolarisRequestHeaders} from '@enigmatis/polaris-common';
+import {FindConditions, In, MoreThan} from 'typeorm';
+import {PolarisFindManyOptions, PolarisFindOneOptions} from '..';
 
 export const getEntitiesIncludingDeletedConditions: FindConditions<any> = {
     deleted: In([true, false]),
@@ -18,19 +18,17 @@ export class FindHandler {
     ) {
         const headers: PolarisRequestHeaders = polarisOptions?.context?.requestHeaders || {};
 
-        let polarisCriteria: any = {};
+        let polarisCriteria: any;
         if (typeof polarisOptions?.criteria === 'string') {
-            polarisCriteria = { where: { id: polarisOptions.criteria } };
+            polarisCriteria = {where: {id: polarisOptions.criteria}};
         } else if (polarisOptions?.criteria instanceof Array) {
-            polarisCriteria = { where: { id: In(polarisOptions.criteria) } };
+            polarisCriteria = {where: {id: In(polarisOptions.criteria)}};
         } else {
             polarisCriteria = polarisOptions?.criteria || {};
         }
 
-        polarisCriteria.where = { ...polarisCriteria.where };
-        if (polarisCriteria.where.deleted === undefined) {
-            polarisCriteria.where.deleted = false;
-        }
+        polarisCriteria.where = {...polarisCriteria.where};
+        this.setDeletedCriteriaIncludingRelations(polarisCriteria);
         if (polarisCriteria.where.dataVersion === undefined && headers.dataVersion) {
             polarisCriteria.where.dataVersion = MoreThan(headers.dataVersion);
         }
@@ -38,5 +36,30 @@ export class FindHandler {
             polarisCriteria.where.realityId = realityIdCriteria(includeLinkedOper, headers);
         }
         return polarisCriteria;
+    }
+
+    private setDeletedCriteriaIncludingRelations(polarisCriteria: any) {
+        if (polarisCriteria.where.deleted === undefined) {
+            polarisCriteria.where.deleted = false;
+
+            if (polarisCriteria.relations) {
+                polarisCriteria.relations.forEach((relation: any) => {
+                    polarisCriteria.where = this.setNestedDeleteFilter(relation, polarisCriteria.where);
+                });
+
+            }
+        }
+    }
+
+    private setNestedDeleteFilter(key: string, object: any) {
+        let result: any;
+        result = object;
+        let deleted = false;
+        let relations = key.split(".");
+        relations.forEach((relation: string ) => {
+            object = object[relation] = {deleted, ...object[relation]};
+        });
+
+        return result;
     }
 }
